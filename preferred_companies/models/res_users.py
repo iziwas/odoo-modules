52e85572981acd068d8d298c3a514a34145a70fd
Iziwas <kevin@couriat.info>
2026-06-27 22:15:59 +0200

    [FIX] User company selection + multiples bugs + add readme.md file

--- preferred_companies/models/res_users.py
+++ preferred_companies/models/res_users.py
@@ -1,12 +1,34 @@
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ResUsers(models.Model):
     _inherit = "res.users"
 
     preferred_company_ids = fields.Many2many(
-        "res.company", "user_id", string="Preferred companies"
+        "res.company",
+        "res_users_preferred_company_rel",
+        "user_id",
+        "company_id",
+        string="Preferred companies",
     )
 
     def _get_preferred_companies(self):
         return self.preferred_company_ids.ids
+
+    @api.constrains("preferred_company_ids")
+    def _check_preferred_companies(self):
+        for user in self:
+            if user.preferred_company_ids - user.company_ids:
+                raise ValidationError(
+                    _("Preferred companies must be among the user's allowed companies.")
+                )
+
+    def write(self, vals):
+        result = super().write(vals)
+        if "company_ids" in vals:
+            for user in self:
+                stale = user.preferred_company_ids - user.company_ids
+                if stale:
+                    user.preferred_company_ids -= stale
+        return result
--- preferred_companies/static/src/js/company_service.js
+++ preferred_companies/static/src/js/company_service.js
@@ -6,19 +6,21 @@ import {patch} from "@web/core/utils/patch";
 import {session} from "@web/session";
 
 patch(companyService, {
-    start(env, { user, router, action }) {
+    start(env, {user, router, action}) {
         const hashCompanyIds = router.current.hash.cids;
         const shouldSelectAll = !hashCompanyIds && !cookie.get("cids");
 
         if (shouldSelectAll) {
-            const allCompanyIds = Object.values(session.user_companies.preferred_companies)
-                .map(id => parseInt(id));
-
-            if (allCompanyIds.length > 0) {
-                const cidsHash = allCompanyIds.join("-");
-                router.current.hash.cids = cidsHash;
+            const preferred = session.user_companies?.preferred_companies;
+            if (preferred?.length > 0) {
+                const defaultCompanyId = session.user_companies.current_company;
+                const ordered =
+                    defaultCompanyId && preferred.includes(defaultCompanyId)
+                        ? [defaultCompanyId, ...preferred.filter((id) => id !== defaultCompanyId)]
+                        : preferred;
+                cookie.set("cids", ordered.join("-"));
             }
         }
-        return super.start(env, { user, router, action });
+        return super.start(env, {user, router, action});
     },
 });
